8186b37832410a4c89425b9f3bfcac81e0560eb1
TonyCioara <tonyangelo9707@gmail.com>
2018-04-09 15:18:14 -0700

    Connected ProfileInfoCell to UI

--- fj-mobile/ViewControllers/ProfileViewController.swift
+++ fj-mobile/ViewControllers/ProfileViewController.swift
@@ -8,7 +8,80 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController {
+class ProfileViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, ImageSelectionDelegate {
     
+    @IBOutlet weak var tableView: UITableView!
     
+    var currentCell: ProfileInfoCell?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "profileInfoCell", for: indexPath) as! ProfileInfoCell
+        
+        cell.emailLabel.text = "testmail@mail.com"
+        cell.firstNameLabel.text = "Tony"
+        cell.lastNameLabel.text = "Cioara"
+        cell.nicknameLabel.text = "TestNickname"
+        cell.delegate = self
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 380
+    }
+    
+    func didClickSelectButton(cell: ProfileInfoCell) {
+        self.currentCell = cell
+        selectImage()
+    }
 }
+
+extension ProfileViewController:  UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func selectImage() {
+        
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        
+        let actionSheet = UIAlertController(title: "Photo Source", message: "Choose a source", preferredStyle: .actionSheet)
+        
+        //        Add camera button
+        actionSheet.addAction(UIAlertAction(title: "Camera", style: .default, handler: { (action: UIAlertAction) in
+            imagePickerController.sourceType = .camera
+            self.present(imagePickerController, animated: true, completion: nil)
+        }))
+        
+        //        Add Photo library button
+        actionSheet.addAction(UIAlertAction(title: "Photo Library", style: .default, handler: { (action: UIAlertAction) in
+            imagePickerController.sourceType = .photoLibrary
+            self.present(imagePickerController, animated: true, completion: nil)
+        }))
+        
+        //        Add cancel button
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        self.present(actionSheet, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        self.currentCell?.profileImageView.image = image
+        
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+}
+
