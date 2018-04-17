8a1745e28935a76a428915fc51177a7dedfdcdcb
TonyCioara <tonyangelo9707@gmail.com>
2018-04-17 15:34:51 -0700

    preparing for demo night release

--- fj-mobile/ViewControllers/CampaignsViewController.swift
+++ fj-mobile/ViewControllers/CampaignsViewController.swift
@@ -28,9 +28,25 @@ class CampaignsViewController: UIViewController, UITableViewDelegate, UITableVie
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.model = generateRandomData()
+//        self.model = generateRandomData()
         
 //        Navigation bar setup
+//        navigationController?.navigationBar.barTintColor = UIColor(red:0.85, green:0.68, blue:0.51, alpha:1.0)
+//        navigationController?.navigationBar.tintColor = UIColor.black
+//
+//        self.tableView.separatorStyle = .none
+//        self.tableView.delegate = self
+//        self.tableView.dataSource = self
+//
+//
+//        self.loadCampaigns {
+//            DispatchQueue.main.async {
+//                self.tableView.reloadData()
+//            }
+//        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
         navigationController?.navigationBar.barTintColor = UIColor(red:0.85, green:0.68, blue:0.51, alpha:1.0)
         navigationController?.navigationBar.tintColor = UIColor.black
         
@@ -44,9 +60,6 @@ class CampaignsViewController: UIViewController, UITableViewDelegate, UITableVie
                 self.tableView.reloadData()
             }
         }
-        
-//        self.tableView.allowsSelection = false
-        
     }
     
 //       Set up tableView
